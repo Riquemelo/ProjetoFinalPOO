c53e0ee37166e0f566d0e1c69a95a0225b454c49
Riquemelo <31356272+Riquemelo@users.noreply.github.com>
2017-11-19 19:10:20 -0200

    Projeto Iniciado,BD com alguns problemas
    Bd não conectando

---
+++ ProjetoFinalPOO/build/web/index.jsp
@@ -0,0 +1,19 @@
+<%@page import="java.sql.DriverManager"%>
+<%@page import="java.sql.Connection"%>
+<%@page import="java.sql.ResultSet"%>
+<%@page import="java.sql.Statement"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import="br.com.fatecpg.project.Database"%>
+<%@page import="br.com.fatecpg.project.User"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        
+        
+    </body>
+</html>
---
+++ ProjetoFinalPOO/src/java/br/com/fatecpg/project/Database.java
@@ -0,0 +1,27 @@
+package br.com.fatecpg.project;
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+public class Database {
+    private static Connection connection;
+    private static Exception connectionException;
+    
+    public static Connection getConnection(){
+        if(connection == null){
+            try{
+                Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+                String url = "jdbc:derby://localhost:1527/ProjetoHealthcare;create=true";
+                connection = DriverManager.getConnection(url);
+                connectionException = null;
+            }catch(Exception ex){
+                connection = null;
+                connectionException = ex;
+            }
+        }
+        return connection;
+    }
+    
+    public static Exception getConnectionException(){
+        return connectionException;
+    }
+}
